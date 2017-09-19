a0b9a4bef89651e7bcddc2365a89ba7ab4b427f1
William Huang <hylbegoat@gmail.com>
2017-09-19 19:28:30 +0800

    fix the bug of releative path

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 #include <QtWidgets>
 
 #include "utils.h"
-#include "logview/logview.h"
 
 #ifdef DEVELOP_MODE
+#include "logview/logview.h"
 #include "watchreload/watchreload.h"
 #endif
 
@@ -15,9 +15,6 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
-    //    QWidget window;
-    //    QMainWindow window;
-    //    QQmlApplicationEngine engine(&window);
     QQmlApplicationEngine engine;
 
 #if defined(DEVELOP_MODE)
@@ -45,30 +42,18 @@ int main(int argc, char *argv[])
     WatchReload reloader(&engine);
     QObject::connect(&filewatcher,&QFileSystemWatcher::directoryChanged,&reloader,&WatchReload::reload);
 
-
-    //    QWidget *main = qobject_cast<QWidget*>(engine.rootObjects().first());
-    //    qDebug() << main;
-
-    //    mainwindow.setCentralWidget(main);
-    //    qDebug() << mainwindow.centralWidget();
-
-    //    QDockWidget *m_logDock = new QDockWidget("test",&window,Qt::Dialog);
-    //    m_logDock->setObjectName("log");
-    //    m_logDock->setAllowedAreas(Qt::TopDockWidgetArea | Qt::BottomDockWidgetArea);
-    //    m_logDock->setFeatures(QDockWidget::AllDockWidgetFeatures);
-    ////    m_logDock->show();
-    ////    main->addDockWidget(Qt::BottomDockWidgetArea, m_logDock);
-    //    m_logDock->show();
-    //    qDebug() << m_logDock->widget();
-
+    // get pointer to ApplicationWindow
     QWindow *qmlWindow = qobject_cast<QWindow*>(engine.rootObjects().first());
-    qDebug() << qmlWindow;
 
+    // create window container
     QWidget *container = QWidget::createWindowContainer(qmlWindow);
     qDebug() << container;
     container->setMinimumSize(qmlWindow->size().width(), qmlWindow->size().height());
 
+
     QWidget *widget = new QWidget();
+
+    // create a layout
     QGridLayout *grid = new QGridLayout(widget);
     grid->addWidget(container,0,0);
 
@@ -76,11 +61,9 @@ int main(int argc, char *argv[])
     m_logDock->setObjectName("log");
     m_logDock->setAllowedAreas(Qt::TopDockWidgetArea | Qt::BottomDockWidgetArea);
     m_logDock->setFeatures(QDockWidget::AllDockWidgetFeatures);
-
-    LogView *m_log = new LogView(true,m_logDock);
-    //m_logDock->setWidget(new QPlainTextEdit("This is a test"));
+    LogView *m_log = new LogView(true); // This is embeded widget
     m_logDock->setWidget(m_log);
-    grid->addWidget(m_logDock,1,0);
+    grid->addWidget(m_logDock,1,0); // place the widget to the top of the widget
     widget->show();
 
 #else
--- src/watchreload/watchreload.cpp
+++ src/watchreload/watchreload.cpp
@@ -1,5 +1,6 @@
 #include <QDebug>
 
+#include "utils.h"
 #include "watchreload.h"
 
 WatchReload::WatchReload(QQmlApplicationEngine *engine)
@@ -14,13 +15,19 @@ void WatchReload::reload()
     qDebug() << "file changes detected";
     if (this->headerObj && this->contentObj) {
 
+        QString contentPath = WATCH_DIR_PATH;
+        QString headerPath = WATCH_DIR_PATH;
+        QString errorPath = WATCH_DIR_PATH;
+        headerPath.prepend("file:").append("/HeaderComponent.qml");
+        contentPath.prepend("file:").append("/ContentComponent.qml");
+        errorPath.prepend("file:").append("/ErrorComponent.qml");
+
         this->headerObj->setProperty("active",false);
         this->contentObj->setProperty("active",false);
         this->engine->clearComponentCache();
-        // 这里需要 调整一下 还是 弄成绝对路径好了
-        // 这里的相对路径会用到 编译目录 不太方便
-        this->headerObj->setProperty("source","../script/qml/HeaderComponent.qml");
-        this->contentObj->setProperty("source","../script/qml/ContentComponent.qml");
+
+        this->headerObj->setProperty("source",headerPath);
+        this->contentObj->setProperty("source",contentPath);
         this->engine->clearComponentCache();
         this->headerObj->setProperty("active",true);
         this->contentObj->setProperty("active",true);
@@ -31,7 +38,7 @@ void WatchReload::reload()
             this->headerObj->setProperty("source","");
 
             // Avoiding incorrectly changes leading it down
-            this->contentObj->setProperty("source","../script/qml/ErrorComponent.qml");
+            this->contentObj->setProperty("source",errorPath);
         } else {
             qDebug() << "Application reloaded successfully";
         }
